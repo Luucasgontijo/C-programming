d40d10e880e57019d0d365d5a821e4737e4de041
Lucas Gontijo  de Moraes <164579296+Luucasgontijo@users.noreply.github.com>
2025-02-08 18:36:04 -0300

    node comments

--- pointers/nodes.c
+++ pointers/nodes.c
@@ -17,20 +17,20 @@ void insertAtBeginning(struct node** pointerToHead, int data){
 }
 void insertAtNthPosition(struct node** pointerToHead, int data, int n){
     struct node* newNode = (struct node*)malloc(sizeof(struct node*));
-    newNode->data = data;
-    newNode->link = NULL;
+    newNode->data = data; //the node that will be inserted receive his data var.
+    newNode->link = NULL; //  the newNode.link receives NULL value. In this moment it is the TAIL, the last item of linked list.
     if (n == 1){
-        newNode->link = *pointerToHead;
-        *pointerToHead = newNode;
-        return;
+        newNode->link = *pointerToHead;     //if we re trying to insert an node at position 1 (head) it link will be link to the actual HEAD, so if there was an head with adress 200, and we change it to this newNode, the newNode.link will be the adress of the old head.
+        *pointerToHead = newNode;   // the head will be now the newNode (in first iteration it is NULL, because there is no head at this moment)
+        return; 
     }
-    struct node* temp2 = *pointerToHead;
+    struct node* temp = *pointerToHead; //if we re trying to insert a new node, and the position we re aiming is not the very first one (1), we define an TEMP node, and make it equal to the actual head.
 
-    for (int i= 0; i <n-2; i++){
-        temp2 = temp2->link;
+    for (int i= 0; i <n-2; i++){   //if we are trying to insert an node in the position 2, this for will not run
+        temp = temp->link;          //if n is a position other than 1 or 2, the temp will be the head.link now, so, the second item
     }
-    newNode->link= temp2->link;
-    temp2->link =newNode;
+    newNode->link= temp->link; // the newNode->link will be equal to the link of the head(head pointing to second), now newNode is pointing to the second as well
+    temp->link =newNode; // the head link now will be the newNode adress
 
 }
 
@@ -55,10 +55,10 @@ int main(){
     printf("\n");
 
     head = NULL;
-    insertAtNthPosition(&head, 2, 1);
-    insertAtNthPosition(&head, 3, 2);
     insertAtNthPosition(&head, 4, 1);
-    insertAtNthPosition(&head, 5, 2);
+    insertAtNthPosition(&head, 6, 2);
+    insertAtNthPosition(&head, 7, 1);
+    insertAtNthPosition(&head, 8, 2);
     printLinkedList(head);
 
 
